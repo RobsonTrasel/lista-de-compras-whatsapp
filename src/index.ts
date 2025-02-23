37a68f3ad5761636fa83b91fdd753d71639ffb54
RobsonTrasel <robson.trasel@gmail.br>
2025-02-23 01:06:05 -0300

    fix: correct type for catch

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ const PORT = process.env.PORT || 3000;
 
 prisma.$connect()
     .then(() => console.log("✅ Conectado ao PostgreSQL"))
-    .catch((err) => console.error("❌ Erro ao conectar ao PostgreSQL:", err));
+    .catch((err: Error) => console.error("❌ Erro ao conectar ao PostgreSQL:", err));
+
 
 // Rota para a Alexa
 app.post("/alexa/shopping-list", handleAlexaRequest);
